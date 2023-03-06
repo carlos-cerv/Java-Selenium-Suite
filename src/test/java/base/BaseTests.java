90652384388b2a6b33afaa6be73f84c98964a6e1
carlos.eduardo <carlos.eduardo@wizeline.com>
2023-03-06 11:47:15 -0600

    06/03/2023 Exercise 1 Locator Menu Validation

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -29,9 +29,34 @@ public class BaseTests {
         driver.quit();
     }
 
+    //Selenium Chapter 3
+    public void exercise1(){
+        System.setProperty("webdriver.chrome.driver","src/resources/mac/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver.manage().window().setSize(new Dimension(375,812));
+        driver.get("https://the-internet.herokuapp.com/");
+
+        //Find Shifting content Link
+        WebElement inputsLink = driver.findElement(By.linkText("Shifting Content"));
+        inputsLink.click();
+
+        //Find Example Menu Element Link
+        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        example1.click();
+
+        //Display Size off Menu Elements
+        List<WebElement> links = driver.findElements(By.tagName("li"));
+        System.out.println(links.size());
+
+
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
     public static void main (String args[]){
         BaseTests test = new BaseTests();
-        test.setUp();
+        test.exercise1();
     }
 
 
