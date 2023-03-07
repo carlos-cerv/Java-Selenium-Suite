78fa6d729d132a3f4fd6224fb76a9d8f881b1935
Carlos Eduardo Cervantes Arteaga <89818143+carlos-cerv@users.noreply.github.com>
2023-03-07 12:36:06 -0600

    07/03/2023 Dropdown Selectors Test

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,14 +6,22 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
 
     private WebDriver driver;
-    private By formAuthenticationLink = By.linkText("Form Authentication");
+    //Adding Global Elements for HomePage Structure
+    //private By formAuthenticationLink = By.linkText("Form Authentication");
 
     public HomePage(WebDriver driver){
         this.driver = driver;
     }
 
     public LoginPage clickFormAuthentication(){
-        driver.findElement(formAuthenticationLink).click();
+        clickLink("Form Authentication");
         return new LoginPage(driver);
+    }
+    public DropdownPage clickDropDown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+    public void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
     }
 }
