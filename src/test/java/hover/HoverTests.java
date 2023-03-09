6acb0bae74c45cfa5a1fe46d78b4b18f1d463213
Carlos Eduardo Cervantes Arteaga <89818143+carlos-cerv@users.noreply.github.com>
2023-03-09 11:04:15 -0600

    09/03/2023 New Updates Advance Interactions v2

--- src/test/java/hover/HoverTests.java
+++ src/test/java/hover/HoverTests.java
@@ -3,11 +3,17 @@ package hover;
 import base.BaseTests;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.*;
+
 public class HoverTests extends BaseTests {
 
     @Test
     public void testHoverUser1 (){
         pages.HoversPage hoverPage = homePage.clickHovers();
         var caption = hoverPage.hoverOverFigure(1);
+        assertTrue(caption.isCaptionDisplayed(),"Caption is not displayed");
+        assertEquals(caption.getTitle(),"name: user1","Capton title incorrect");
+        assertEquals(caption.getLinkText(),"View profile","Caption link text incorrect");
+        assertTrue(caption.getLink().endsWith("/users/1"),"Link Incorrect");
     }
 }
